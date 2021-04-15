733d3d54bd896fddda988939c57c6d43542fa4da
Benjamin Wollenweber <ich@wollis-page.de>
2021-04-15 23:33:59 +0200

    Add own alert dialog

---
+++ app/src/main/java/de/wollis_page/gibsonos/helper/AlertDialog.kt
@@ -0,0 +1,52 @@
+package de.wollis_page.gibsonos.helper
+
+import android.app.AlertDialog
+import android.app.AlertDialog.Builder
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import de.wollis_page.gibsonos.R
+import de.wollis_page.gibsonos.activity.GibsonOsActivity
+import de.wollis_page.gibsonos.dto.DialogItem
+
+class AlertDialog(val context: GibsonOsActivity, val title: String, val items: ArrayList<DialogItem>) {
+    var layout: Int = R.layout.base_alert_dialog_item
+//    this.items.toArray(arrayOfNulls<String>(0)
+    fun show(): AlertDialog? {
+        val adapter: ListAdapter = object : ArrayAdapter<DialogItem>(
+            this.context,
+            this.layout,
+            R.id.name,
+            this.items
+        ) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view: View = super.getView(position, convertView, parent)
+
+                val item = items[position]
+                val icon = item.icon
+
+                if (icon != null) {
+                    view.findViewById<ImageView>(R.id.icon).setImageResource(icon)
+                }
+
+                view.findViewById<TextView>(R.id.name).text = item.text
+
+                return view
+            }
+        }
+
+        return Builder(this.context)
+            .setTitle(this.title)
+            .setAdapter(adapter) { _, which ->
+                val item = this.items[which]
+                val onClick = item.onClick ?: return@setAdapter
+
+                if (item.children != null && !item.fireOnClickOnExpand) {
+                    return@setAdapter
+                }
+
+                onClick()
+            }
+            .show()
+    }
+}
