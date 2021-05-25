4fe40d771dfe26ffd4a95c54a6358be9b15fb5cf
Benjamin Wollenweber <ich@wollis-page.de>
2021-05-25 23:23:03 +0200

    Add chromecast

---
+++ app/src/main/java/de/wollis_page/gibsonos/helper/CastOptionsProvider.kt
@@ -0,0 +1,18 @@
+package de.wollis_page.gibsonos.helper
+
+import android.content.Context
+import com.google.android.gms.cast.framework.CastOptions
+import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
+
+class CastOptionsProvider : OptionsProvider {
+    override fun getCastOptions(context: Context): CastOptions {
+        return CastOptions.Builder()
+            .setReceiverApplicationId("1EEA092A")
+            .build()
+    }
+
+    override fun getAdditionalSessionProviders(context: Context?): List<SessionProvider>? {
+        return null
+    }
+}
